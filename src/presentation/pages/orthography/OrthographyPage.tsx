dcd303cc271051d4a4afe2d1e8c7081eeb92734c
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-01-30 16:04:10 -0300

    Desenhando a tela do chat

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,3 +1,15 @@
+import { GptMessage, MyMessage } from "../../components";
+
 export const OrthographyPage = () => {
-  return <div>OrthographyPage</div>;
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-20">
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones" />
+          <MyMessage text="Hola Mundo" />
+        </div>
+      </div>
+    </div>
+  );
 };
