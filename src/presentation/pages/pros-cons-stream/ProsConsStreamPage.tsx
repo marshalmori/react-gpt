9fbe4a5c682adc3f70a8954ed663e55282aedfa9
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2024-02-06 23:15:55 -0300

    Leer respuesta de un stream

--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,3 +1,61 @@
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+import { prosConsStreamUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export const ProsConsStreamPage = () => {
-  return <div>ProsConsStreamPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    // TODO: UseCase
+    await prosConsStreamUseCase(text);
+
+    setIsLoading(false);
+
+    // TODO: Añadir el mensaje de isGPT en true
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-20">
+          {/* Bienvenida */}
+          <GptMessage text="Que deseas comparar hoy?" />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text="Esto es de OpenAI" />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aquí lo que deseas"
+        disableCorrections
+      />
+    </div>
+  );
 };
--- src/presentation/pages/pros-cons/ProsConsPage.tsx
+++ src/presentation/pages/pros-cons/ProsConsPage.tsx
@@ -1,3 +1,62 @@
+import { useState } from "react";
+import {
+  GptMessage,
+  MyMessage,
+  TextMessageBox,
+  TypingLoader,
+} from "../../components";
+import { prosConsUseCase } from "../../../core/use-cases";
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
 export const ProsConsPage = () => {
-  return <div>ProsConsPage</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async (text: string) => {
+    setIsLoading(true);
+    setMessages((prev) => [...prev, { text: text, isGpt: false }]);
+
+    // TODO: UseCase
+    const { ok, content } = await prosConsUseCase(text);
+    setIsLoading(false);
+
+    if (!ok) return;
+
+    setMessages((prev) => [...prev, { text: content, isGpt: true }]);
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-20">
+          {/* Bienvenida */}
+          <GptMessage text="Puedes escribir lo que seia que quieres que compare y te de mis puntos de vista." />
+
+          {messages.map((message, index) =>
+            message.isGpt ? (
+              <GptMessage key={index} text={message.text} />
+            ) : (
+              <MyMessage key={index} text={message.text} />
+            )
+          )}
+
+          {isLoading && (
+            <div className="col-start-1 col-end-12 fade-in">
+              <TypingLoader />
+            </div>
+          )}
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="Escribe aquí lo que deseas"
+        disableCorrections
+      />
+    </div>
+  );
 };
